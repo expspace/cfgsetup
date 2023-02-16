a87f27a2b39df6caa4331152c78ceec5054455ea
expspace <sebshah@hotmail.com>
2023-02-16 14:28:23 -0500

    Update gitconfig

--- gitconfig.sh
+++ gitconfig.sh
@@ -5,10 +5,12 @@ git config --global editor.core 'nano'
 git config --global alias.s 'status -s'
 git config --global alias.a 'add'
 git config --global alias.aa 'add .'
-git config --global alias.pu push
 git config --global alias.br branch
+git config --global alias.brd 'branch -d'
 git config --global alias.co checkout
+git config --global alias.cob 'checkout -b'
 git config --global alias.ci commit
+git config --global alias.cim 'commit -m'
 git config --global alias.pl pull
 git config --global alias.ls 'log --pretty="format:%C(yellow)%h %C(cyan)%ad %C(green)%<(7,trunc)%aN %C(auto,reset)%s%C(auto,red)% gD% D" --date=short'
 git config --global alias.stats 'shortlog -sne'
