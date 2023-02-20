28e242b2ff9651b40f43b3a63e65caa5e6a86766
expspace <sebshah@hotmail.com>
2023-02-20 15:56:19 -0500

    Update gitconfig

--- gitconfig.sh
+++ gitconfig.sh
@@ -10,7 +10,7 @@ git config --global alias.brd 'branch -d'
 git config --global alias.co checkout
 git config --global alias.cob 'checkout -b'
 git config --global alias.ci commit
-git config --global alias.cim 'commit -m'
+git config --global alias.cm 'commit -m'
 git config --global alias.pl pull
 git config --global alias.ls 'log --pretty="format:%C(yellow)%h %C(cyan)%ad %C(green)%<(7,trunc)%aN %C(auto,reset)%s%C(auto,red)% gD% D" --date=short'
 git config --global alias.stats 'shortlog -sne'
