d4a048dde133403d9ca787b345fb6a6a6c18707d
expspace <sebshah@hotmail.com>
2023-03-27 15:02:38 -0400

    Update gitconfig

--- gitconfig.sh
+++ gitconfig.sh
@@ -9,17 +9,20 @@ git config --global editor.core 'nano'
 ## commit
 git config --global alias.s 'status -sb'
 git config --global alias.a 'add'
-git config --global alias.aa 'add .'
+git config --global alias.aa 'add --all'
 git config --global alias.c commit
 git config --global alias.cm 'commit -m'
 git config --global alias.ca 'commit --amend'
 
 ## branch
+git config --global alias.f fetch
 git config --global alias.br branch
 git config --global alias.brd 'branch -D'
+git config --global alias.brdm '!git branch --merged | grep -Ev "devel|future" | xargs git branch -d'
 git config --global alias.co checkout
 git config --global alias.cob 'checkout -b'
 git config --global alias.pl pull
+git config --global alias.pll 'pull origin future'
 
 ## misc
 git config --global alias.ls 'log --pretty="format:%C(yellow)%h %C(cyan)%ad %C(green)%<(7,trunc)%aN %C(auto,reset)%s%C(auto,red)% gD% D" --date=short'
